ef826b9303ae2b963916b4909abf0b1cdbe860ae
GiuseppeCutrano <giuseppecutrano.info@gmail.com>
2020-10-27 15:27:38 +0100

    palindroma fatto

---
+++ palindroma/script.js
@@ -0,0 +1,29 @@
+
+//prompt utente
+var parolaUtente = prompt('Inserisci una parola');
+console.log(parolaUtente);
+//utilizzare la funzione per sapere se la parola inserita è palindroma
+function palindroma (word) {
+    var lettereParola = word.split("");
+    console.log(lettereParola);
+
+    var lettereParolaReverse = lettereParola.reverse();
+    console.log(lettereParolaReverse);
+
+    var parolaInversa = lettereParolaReverse.join('');
+    console.log(parolaInversa);
+    return parolaInversa;
+}
+
+var palindroma = palindroma(parolaUtente);
+console.log(palindroma);
+
+// stampare un messaggio appropriato
+
+if (parolaUtente == palindroma) {
+    alert("La parola è palindroma");
+    console.log('La parola è palindroma');
+} else {
+    alert("La parola non è palindroma")
+    console.log('La parola non è palindroma');
+}
